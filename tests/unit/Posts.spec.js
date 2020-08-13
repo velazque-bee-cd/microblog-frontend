7f4231e49746358c69440870d9a98e24a19a05fa
Nelson Velazquez <velazque@users.noreply.github.com>
2020-08-13 09:34:48 -0500

    Update Posts.spec.js

--- tests/unit/Posts.spec.js
+++ tests/unit/Posts.spec.js
@@ -2,7 +2,9 @@ import Posts from '@/views/Posts.vue'
 
 describe('Posts.vue', () => {
   it('has a created hook', () => {
+    
     expect(typeof Posts.created).toBe('function')
+    
   })
   it('sets the correct default data', () => {
     expect(typeof Posts.data).toBe('function')
